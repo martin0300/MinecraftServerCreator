abc1d88f8d14e60745ea8f65fe6ea2cf73247d8a
martin0300 <78685169+martin0300@users.noreply.github.com>
2022-06-16 22:23:20 +0200

    Beta release V1.1

--- minecraftservercreator.py
+++ minecraftservercreator.py
@@ -126,6 +126,7 @@ def menu():
     print("Choices:")
     print("-create")
     print("-help")
+    print("-pluginmanager")
     print("-exit")
     choice = input(">")
     if choice == "create":
@@ -138,6 +139,8 @@ def menu():
         mscexit()
     elif choice == "debug":
         mscdebug()
+    elif choice == "pluginmanager":
+        pluginmanager()
     else:
         print("Not a choice!")
         menu()
@@ -277,6 +280,7 @@ def mschelp():
     print("Menu:")
     print("-create (starts server creation)")
     print("-help (shows this screen)")
+    print("-pluginmanager (starts plugin manager)")
     print("-exit (exits the program)")
     print("Press enter to return!")
     input()
@@ -286,7 +290,7 @@ def getostype():
     return platform.system()
 
 def mscexit():
-    exit(0)
+    sys.exit(0)
 
 ##download database
 def downloaddatabase():
@@ -568,9 +572,387 @@ def configinstall():
     except IndexError:
         confighelp()
 
+def pluginmanager():
+    print("MSC Plugin Manager V" + plugmanver)
+    print("Currently " + str(len(pluginlists)) + " lists available!")
+    print("Choices:")
+    print("-search\n-searchid\n-addtolist\n-listlists\n-listlist\n-newlist\n-removelist\n-removefromlist\n-help\n-menu")
+    choice = input(">")
+    if choice == "search":
+        searchplugins(None)
+        pluginmanager()
+    elif choice == "searchid":
+        print("ID:")
+        idsearch = input("#")
+        searchplugins(idsearch)
+        pluginmanager()
+    elif choice == "addtolist":
+        print("ID to add:")
+        toaddid = input("#")
+        if isnoneorempty(toaddid) == True:
+            print("Nothing is entered!")
+            pluginmanager()
+        listmanager(choice, toaddid)
+        pluginmanager()
+    elif choice == "listlists":
+        listmanager(choice)
+        pluginmanager()
+    elif choice == "listlist":
+        listmanager(choice)
+        pluginmanager()
+    elif choice == "newlist":
+        listmanager(choice)
+        pluginmanager()
+    elif choice == "removelist":
+        listmanager(choice)
+        pluginmanager()
+    elif choice == "removefromlist":
+        listmanager(choice)
+        pluginmanager()
+    elif choice == "help":
+        plugmanhelp()
+        pluginmanager()
+    elif choice == "menu":
+        menu()
+    else:
+        print("Not a choice!")
+        pluginmanager()
+
+def listmanager(task, id = None):
+    if task == "loadlists":
+        try:
+            print("Loading plugin lists...")
+            if os.path.isfile("pluginlist.json") == False:
+                print("No plugin list file found!")
+                print("Creating new one...")
+                newpluginlist = {}
+                with open("pluginlist.json", "w", encoding="utf-8") as newpluginfile:
+                    json.dump(newpluginlist, newpluginfile)
+                print("Created plugin list file!")
+            with open("pluginlist.json", "r") as pluginfile:
+                pluginlistload = json.load(pluginfile)
+            return pluginlistload
+        except (UnicodeDecodeError, ValueError):
+            print("Plugin list cannot be loaded because of a corrupted file or incompatible character! Exiting!")
+            mscexit()
+    elif task == "addtolist":
+        print("To which list?")
+        for x in pluginlists:
+            print("-" + x)
+        print('Choose a name or type "new"!')
+        choiceaddlist = input(">")
+        if isnoneorempty(choiceaddlist) == True:
+            print("Nothing is entered!")
+            listmanager(task, id)
+        if choiceaddlist == "new":
+            if listmanager("newlist") == False:
+                print("Try again!")
+                listmanager(task, id)
+            listmanager(task, id)
+        else:
+            if pluginlists.get(choiceaddlist, None) == None:
+                print("List does not exist!")
+            info = searchplugins(id, True)
+            if info == False:
+                print("Plugin cannot be added!")
+                return
+            elif info == "badid":
+                return
+            pluginlists.get(choiceaddlist).append(info)
+            listmanager("savelist")
+            print("Added to list!")
+            return
+    elif task == "newlist":
+        print("Name:")
+        newname = input("?")
+        if isnoneorempty(newname) == True:
+            print("No name entered!")
+            return False
+        if pluginlists.get(newname, None) != None:
+            print("List already exist!")
+            return False
+        print("Creating list...")
+        pluginlists.update({newname: []})
+        listmanager("savelist")
+        print("List created!")
+        return True
+    elif task == "savelist":
+        print("Saving list...")
+        with open("pluginlist.json", "w", encoding="utf-8") as savelist:
+            json.dump(pluginlists, savelist, ensure_ascii=False)
+        print("List saved!")
+        return
+    elif task == "listlists":
+        if len(pluginlists) == 0:
+            print("No available lists!")
+            return False
+        print("Available lists:")
+        for x in pluginlists:
+            print("-" + x + " (" + str(len(pluginlists.get(x))) + " plugins)")
+        return
+    elif task == "listlist":
+        if listmanager("listlists") == False:
+            return
+        else:
+            print("Choose a name:")
+            llname = input("?")
+            if isnoneorempty(llname) == True:
+                print("Nothing is entered!")
+                return
+            if pluginlists.get(llname, None) == None:
+                print("List does not exist!")
+                return
+            if len(pluginlists.get(llname)) == 0:
+                print("List is empty!")
+                return
+            print(llname + ":")
+            i = 1
+            for item in pluginlists.get(llname):
+                print(str(i) + ":")
+                print("Name: " + item.get("name"))
+                if item.get("tag") != None:
+                    print("Tag: " + item.get("tag"))
+                if item.get("contributors") != None:
+                    print("Contributors: " + item.get("contributors"))
+                print("Likes: " + str(item.get("likes")))
+                print("Downloads: " + str(item.get("downloads")))
+                print("ID: " + str(item.get("id")))
+                print("File type: " + item.get("file_type"))
+                i += 1
+            return
+    elif task == "removelist":
+        if listmanager("listlists") == False:
+            return
+        print("Choose a list to remove:")
+        removelistname = input("?")
+        if isnoneorempty(removelistname) == True:
+            print("Nothing is entered!")
+            return
+        if pluginlists.get(removelistname, None) == None:
+            print("List does not exist!")
+            return
+        print("Do you want to remove " + removelistname + "? (yes/no)")
+        removechoice = input(">")
+        if removechoice == "yes":
+            print("Removing list...")
+            pluginlists.pop(removelistname)
+            listmanager("savelist")
+            print("List removed!")
+            return
+        else:
+            return
+    elif task == "removefromlist":
+        if listmanager("listlists") == False:
+            return
+        print("Choose a list to remove from:")
+        removefrominput = input("?")
+        if isnoneorempty(removefrominput) == True:
+            print("Nothing is entered!")
+            return
+        if pluginlists.get(removefrominput, None) == None:
+            print("List does not exist!")
+            return
+        if len(pluginlists.get(removefrominput)) == 0:
+            print("List is empty!")
+            return
+        print(removefrominput + ":")
+        i = 1
+        for item in pluginlists.get(removefrominput):
+            print(str(i) + ":")
+            print("Name: " + item.get("name"))
+            if item.get("tag") != None:
+                print("Tag: " + item.get("tag"))
+            if item.get("contributors") != None:
+                print("Contributors: " + item.get("contributors"))
+            print("Likes: " + str(item.get("likes")))
+            print("Downloads: " + str(item.get("downloads")))
+            print("ID: " + str(item.get("id")))
+            print("File type: " + item.get("file_type"))
+            i += 1
+        i -= 1
+        print("Choose a number to remove:")
+        removenum = input("#")
+        try:
+            if isnoneorempty(removenum) == True:
+                print("Nothing is entered!")
+                return
+            if i < int(removenum):
+                print("Result does not exist!")
+                return
+            if int(removenum) < 1:
+                print("Invalid number!")
+                return
+            print("Do you want to remove number " + str(removenum) + "? (yes/no)")
+            removelistitem = input("?")
+            if removelistitem == "yes":
+                print("Removing item...")
+                pluginlists.get(removefrominput).pop(int(removenum) - 1)
+                listmanager("savelist")
+                print("Removed item!")
+                return
+            else:
+                return
+        except ValueError:
+            print("Not a number!")
+            return
+
+def searchplugins(id, getinfo = False):
+    if getinfo == True:
+        try:
+            responsegetinfoid = requests.get(apiurls.get("searchid") + id).json()
+            if responsegetinfoid.get("external") == True:
+                return False
+            if responsegetinfoid.get("file").get("type") == "external":
+                return False
+            name = responsegetinfoid.get("name")
+            if responsegetinfoid.get("tag", None) != None:
+                tag = responsegetinfoid.get("tag")
+            else:
+                tag = None
+            if responsegetinfoid.get("contributors", None) != None:
+                contributors = responsegetinfoid.get("contributors")
+            else:
+                contributors = None
+            likes = responsegetinfoid.get("likes")
+            downloads = responsegetinfoid.get("downloads")
+            id = responsegetinfoid.get("id")
+            if responsegetinfoid.get("external") != True:
+                if responsegetinfoid.get("file").get("type") != "external":
+                    file_type = responsegetinfoid.get("file").get("type")
+                else:
+                    file_type = None
+            else:
+                file_type = None
+            info = {
+                "name": name,
+                "tag": tag,
+                "contributors": contributors,
+                "likes": likes,
+                "downloads": downloads,
+                "id": id,
+                "file_type": file_type
+            }
+            return info
+        except (TypeError, AttributeError):
+            print("Invalid ID!")
+            return "badid"
+    elif id == None:
+        print("Query:")
+        searchquery = input(">")
+        if isnoneorempty(searchquery) == True:
+            print("Nothing is entered!")
+            return
+        print("How many results?")
+        searchsize = input("#")
+        responsesearch = requests.get(apiurls.get("search") + searchquery + "?field=name&size=" + searchsize).json()
+        print(apiurls.get("search") + searchquery + "?field=name&size=" + searchsize)
+        i = 1
+        for result in responsesearch:
+            print(str(i) + ":")
+            print("Name: " + result.get("name"))
+            if result.get("tag", None) != None:
+                print("Tag: " + result.get("tag"))
+            if result.get("contributors", None) != None:
+                print("Contributors: " + result.get("contributors"))
+            print("Likes: " + str(result.get("likes")))
+            print("Downloads: " + str(result.get("downloads")))
+            print("ID: " + str(result.get("id")))
+            if result.get("external") != True:
+                if result.get("file").get("type") != "external":
+                    print("File type: " + result.get("file").get("type"))
+            if result.get("external") == True:
+                print("Can be downloaded: no")
+            else:
+                if result.get("file").get("type") == "external":
+                    print("Can be downloaded: no")
+                else:
+                    print("Can be downloaded: yes")
+            i += 1
+        print(str(i)) ##remove this
+        i -= 1
+        if i == 0:
+            print("Nothing found! :(")
+            return
+        print("Add one to the list? (yes/no)")
+        addtolist = input("?")
+        if addtolist == "yes":
+            print("Which one? (result number)")
+            resultnumber = input("#")
+            try:
+                if isnoneorempty(resultnumber) == True:
+                    print("Nothing is entered!")
+                    return
+                if i < int(resultnumber):
+                    print("Result does not exist!")
+                    return
+                if int(resultnumber) < 1:
+                    print("Invalid number!")
+                    return
+                listmanager("addtolist", str(responsesearch[int(resultnumber) - 1].get("id")))
+                return
+            except ValueError:
+                print("Not a number!")
+                return
+        else:
+            return
+    else:
+        if isnoneorempty(id) == True:
+            print("No id is entered!")
+            return
+        searchidinput = id
+        responsesearchid = requests.get(apiurls.get("searchid") + searchidinput).json()
+        try:
+            print("Name: " + responsesearchid.get("name"))
+            if responsesearchid.get("tag", None) != None:
+                print("Tag: " + responsesearchid.get("tag"))
+            if responsesearchid.get("contributors", None) != None:
+                print("Contributors: " + responsesearchid.get("contributors"))
+            print("Likes: " + str(responsesearchid.get("likes")))
+            print("Downloads: " + str(responsesearchid.get("downloads")))
+            print("ID: " + str(responsesearchid.get("id")))
+            if responsesearchid.get("external") != True:
+                if responsesearchid.get("file").get("type") != "external":
+                    print("File type: " + responsesearchid.get("file").get("type"))
+            if responsesearchid.get("external") == True:
+                print("Can be downloaded: no")
+            else:
+                if responsesearchid.get("file").get("type") == "external":
+                    print("Can be downloaded: no")
+                else:
+                    print("Can be downloaded: yes")
+            if responsesearchid.get("external") == True:
+                return
+            if responsesearchid.get("file").get("type") == "external":
+                return
+            print("Add it to the list? (yes/no)")
+            addtolist = input("?")
+            if addtolist == "yes":
+                listmanager("addtolist", str(responsesearchid.get("id")))
+                return
+            else:
+                return
+        except TypeError:
+            print("Invalid ID!")
+        return
+
+def plugmanhelp():
+    print("Plugin manager help:")
+    print("-search (search plugins)\n-searchid (search for plugins by id)\n-addtolist (adds plugin id to plugin list)\n"
+          "-listlists (lists created plugin lists)\n-listlist (list plugins stored in selected list)\n-newlist (creates a new plugin list)\n-removelist (delete an existing list)"
+          "\n-removefromlist (removes plugin id from plugin list)\n-help (shows this screen)\n-menu (goes back to menu)")
+    print("Press enter to return!")
+    input()
+    return
+
 ostype = getostype()
-ver = "1.0"
+ver = "1.1"
+plugmanver = "1.0"
 database = {"versions": {}, "urls": {"craftbukkit": {}, "spigot": {}, "vanilla": {}}}
+pluginlists = listmanager("loadlists")
+apiurls = {
+    "search": "https://api.spiget.org/v2/search/resources/",
+    "searchid" : "https://api.spiget.org/v2/resources/"
+}
 
 if __name__ == "__main__":
     init()

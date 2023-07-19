f519dd2e1ea5d2b0022cd3df828b7fb07a3bebe3
martin0300 <78685169+martin0300@users.noreply.github.com>
2023-07-19 15:25:01 +0200

    fix license spacing

--- minecraftservercreator.py
+++ minecraftservercreator.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2023  Martin Magyar
+    Copyright (C) 2023  Martin Magyar
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
